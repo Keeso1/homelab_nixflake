c264be7ee91d17f2de11242a9bd47f52b2372d47
isac <isac.lindh01@gmail.com>
2026-09-10 21:39:27 +0200

    make cyberhome to tailscale exitnode

--- modules/tailscale.nix
+++ modules/tailscale.nix
@@ -8,6 +8,12 @@
   networking.firewall.trustedInterfaces = [ "tailscale0" ];
   networking.firewall.checkReversePath = "loose";
 
+  # Required for exit-node / subnet-router traffic to actually be forwarded.
+  boot.kernel.sysctl = {
+    "net.ipv4.ip_forward" = 1;
+    "net.ipv6.conf.all.forwarding" = 1;
+  };
+
   # First-time setup on each host: sudo tailscale up
   # (add --authkey=... or feed one via secrets/ once sops-nix/agenix is in place).
 }
