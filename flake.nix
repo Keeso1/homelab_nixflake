12ebac2859cadf2c15e524a5c4ac73c9ed5e4947
isac-cloudhome <isac.lindh01@gmail.com>
2026-09-12 19:58:21 +0200

    added zen browser, jq and gimp

--- flake.nix
+++ flake.nix
@@ -7,9 +7,10 @@
       url = "github:noctalia-dev/noctalia";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    zen-browser.url = "github:0xc000022070/zen-browser-flake";
   };
 
-  outputs = { self, nixpkgs, noctalia, ... }: {
+  outputs = { self, nixpkgs, noctalia, zen-browser, ... }: {
     nixosConfigurations = {
       cyberhome = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
@@ -21,7 +22,7 @@
 
       cloudhome = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
-        specialArgs = { inherit noctalia; };
+        specialArgs = { inherit noctalia zen-browser; };
         modules = [
           ./hosts/cloudhome/configuration.nix
         ];
--- hosts/cloudhome/configuration.nix
+++ hosts/cloudhome/configuration.nix
@@ -2,7 +2,7 @@
 # your system. Help is available in the configuration.nix(5) man page, on
 # https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
 
-{ config, pkgs, lib, noctalia, ... }:
+{ config, pkgs, lib, noctalia, zen-browser, ... }:
 
 {
   imports =
@@ -133,6 +133,9 @@
      lazydocker
      bibata-cursors
      ripgrep
+     jq
+     gimp
+     zen-browser.packages."${pkgs.system}".default
    ];
 
   # Some programs need SUID wrappers, can be configured further or are
